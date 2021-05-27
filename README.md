7a3beb911414d5fa7c9489f6e18e4649c5dc44f8
jianmin-chen <goshdarnedspam@gmail.com>
2021-05-27 16:25:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# SoupJS
+
+## About
+SoupJS is a JavaScript library built for simple web scraping.
+
+## Installation
+To install using `npm`, run:
+```
+npm install soupjs
+```
+
+## Usage
+Here's a basic example of using SoupJS:
+```javascript
+const soupjs = require("soupjs");
+soupjs.scrape(url).then(res => {
+    // Search for HTMl elements belonging to CSS selector
+    let pTags = res.findAll("p");
+    console.log(pTags);
+    /*
+        => [ Tag {
+            tag: 'p',
+            text: 'text',
+            attrs: {'class': ['foo', 'bar', 'baz'], ...},
+            htmlContent: 'some text',
+            document: HTMLHtmlElement {}
+        }, ...]
+     */
+}).catch(err => console.log(err));
+```
+For more information on using SoupJS, check out the [documentation](https://github.com/jianmin-chen/soupjs).
+
+## Contributing
+Bug reports and pull requests are welcome on GitHub at [https://github.com/jianmin-chen/soupjs](https://github.com/jianmin-chen/soupjs). This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](http://contributor-covenant.org) code of conduct.
+
+## License
+The theme is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
---
+++ src/index.js
@@ -0,0 +1,112 @@
+/*
+ * Import necessary libraries
+ * node-fetch: Filler for the browser-supported fetch API
+ * fs: Node's filesystem module
+ * jsdom: Filler for the browser-supported DOM
+ */
+const fetch = require("node-fetch");
+const readFile = require("fs").readFile;
+const JSDOM = require("jsdom").JSDOM;
+
+const MULTI_VALUED_ATTRIBUTES = ["class", "rel", "rev", "accept-charset", "headers", "accesskey"];
+let STRIP = (text) => text.replace(/[\n\r]+|[\s]{2,}/g, " ").trim();
+
+class Tag {
+    constructor(element) {
+        this.tag = element.tagName.toLowerCase();
+        this.text = STRIP(element.textContent) || STRIP(element.value);
+        this.attrs = {};
+        Array.from(element.attributes).forEach(attr => {
+            if (MULTI_VALUED_ATTRIBUTES.includes(attr.nodeName)) {
+                // Split multi-valued attributes into a array
+                this.attrs[attr.nodeName] = attr.nodeValue.split(" ");
+            } else {
+                this.attrs[attr.nodeName] = attr.nodeValue;
+            }
+        });
+        this.htmlContent = element.innerHTML;
+        this.document = new JSDOM(element.innerHTML).window.document.documentElement;
+    }
+
+    getAttr(attr) {
+        return this.attrs[attr];
+    }
+
+    find(selector) {
+        const query = this.document.querySelector(selector);
+        if (query) {
+            // Return Tag object if selector is used on page
+            return new Tag(query);
+        }
+        return null;
+    }
+
+    findAll(selector, limit=null) {
+        let tags = [];
+        let query = Array.from(this.document.querySelectorAll(selector));
+        if (limit) {
+            // Limit query elements by slicing
+            query = query.slice(...limit);
+        }
+        query.forEach(element => tags.push(new Tag(element)));
+        return tags;
+    }
+}
+
+class Soup {
+    constructor(content) {
+        this.htmlContent = content;
+        this.document = new JSDOM(content).window.document.documentElement;
+        this.text = this.document.textContent;
+    }
+
+    find(selector) {
+        const query = this.document.querySelector(selector);
+        if (query) {
+            // Return Tag object if selector is used on page
+            return new Tag(query);
+        }
+        return null;
+    }
+
+    findAll(selector, limit=null) {
+        let tags = [];
+        let query = Array.from(this.document.querySelectorAll(selector));
+        if (limit && query.length < limit) {
+            // Limit query elements by slicing
+            query = query.slice(...limit);
+        }
+        query.forEach(element => tags.push(new Tag(element)));
+        return tags;
+    }
+}
+
+const scrape = async (location, filetype="url") => {
+    return new Promise((resolve, reject) => {
+        filetype = filetype.toLowerCase();
+        if (filetype == "url") {
+            // User is trying to scrape data from URL
+            fetch(location).then(res => res.text()).then(html => resolve(new Soup(html))).catch(err => reject(err));
+        } else if (filetype == "file") {
+            // User is trying to scrape data from file
+            readFile(location, "utf8", (err, data) => {
+                if (err) {
+                    // Error reading file
+                    reject(err);
+                } else {
+                    resolve(new Soup(data));
+                }
+            });
+        } else {
+            // filetype parameter is invalid
+            reject(`Filetype parameter is invalid and should either be "url" or "file"`);
+        }
+    });
+};
+
+// Exports
+module.exports = {
+    Tag: Tag,
+    Soup: Soup,
+    scrape: scrape
+};
